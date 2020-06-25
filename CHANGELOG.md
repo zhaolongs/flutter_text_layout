4e2576afc7e3bf6272530a4af0e8efebd1e8f052
zhaolong <928343994@qq.com>
2020-06-25 22:51:44 +0800

    ## [0.0.2] 文本标签Tag增加点击事件

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+## [0.0.2] 文本标签Tag增加点击事件
+
 ## [0.0.1] - TODO: Add release date.
 
 * TODO: Describe initial release.
--- lib/src/text_main_layout_page.dart
+++ lib/src/text_main_layout_page.dart
@@ -11,11 +11,14 @@ class TextLayoutMainPage extends StatefulWidget {
   EdgeInsets margins;
   EdgeInsets padding;
 
+  Function(String value) tagClickCallback;
+
   TextLayoutMainPage(this.textLayoutModelList,
       {this.margins:
-          const  EdgeInsets.only(left: 6, right: 6, top: 4, bottom: 4),
+          const EdgeInsets.only(left: 6, right: 6, top: 4, bottom: 4),
       this.padding =
-          const EdgeInsets.only(left: 6, right: 6, top: 4, bottom: 4)});
+          const EdgeInsets.only(left: 6, right: 6, top: 4, bottom: 4),
+      this.tagClickCallback});
 
   @override
   _PageState createState() => _PageState();
@@ -33,44 +36,62 @@ class _PageState extends State<TextLayoutMainPage> {
     for (TextLayoutModel textModel in widget.textLayoutModelList) {
       ///显示文字的大小
       double fontSize = textModel.textSize;
+
       ///显示文字的颜色
-      Color fontColor = (textModel.textColor==null||textModel.isTextColorRandom)?getRandomColor():textModel.textColor;
+      Color fontColor =
+          (textModel.textColor == null || textModel.isTextColorRandom)
+              ? getRandomColor()
+              : textModel.textColor;
+
       ///背景颜色
       Color backgroundColor = textModel.textBackgroundColor;
+
       ///背景模式
       TextLayoutBackgroundModel backgroundModel = textModel.backgroundModel;
+
       ///文字前面的小圆点的颜色
-      Color iconColor = textModel.iconColor??getRandomColor();
+      Color iconColor = textModel.iconColor ?? getRandomColor();
+
       ///背景的四个角的圆角
       double backGroundRadius = textModel.backGroundRadius;
+
       ///文字前面的小圆点的半径
       double radius = fontSize / 10 * 8;
 
-      Widget cirPoint  = Container();
-      if(textModel.isShowCirPoint){
-       cirPoint =Container(
-         margin: EdgeInsets.only(left: widget.margins.left),
-         width: radius,
-         height: radius,
-         decoration: BoxDecoration(
-             color: iconColor,
-             borderRadius: BorderRadius.all(Radius.circular(9)),
-             boxShadow: [
-               BoxShadow(color: iconColor, offset: Offset(radius/10, radius/4),blurRadius: 0.2),
-               BoxShadow(color: iconColor, offset: Offset(-radius/10, radius/4),blurRadius: 0.2)
-             ]),
-       );
+      Widget cirPoint = Container();
+      if (textModel.isShowCirPoint) {
+        cirPoint = Container(
+          margin: EdgeInsets.only(left: widget.margins.left),
+          width: radius,
+          height: radius,
+          decoration: BoxDecoration(
+              color: iconColor,
+              borderRadius: BorderRadius.all(Radius.circular(9)),
+              boxShadow: [
+                BoxShadow(
+                    color: iconColor,
+                    offset: Offset(radius / 10, radius / 4),
+                    blurRadius: 0.2),
+                BoxShadow(
+                    color: iconColor,
+                    offset: Offset(-radius / 10, radius / 4),
+                    blurRadius: 0.2)
+              ]),
+        );
       }
 
       Row row = Row(
         mainAxisSize: MainAxisSize.min,
         children: [
           ///文字前的小图片
-          textModel.imag??Container(),
+          textModel.imag ?? Container(),
+
           ///文字前的icon,
-          textModel.icon??Container(),
+          textModel.icon ?? Container(),
+
           ///文字前的小圆点
           cirPoint,
+
           ///文字内容
           Container(
             margin: widget.margins,
@@ -82,52 +103,57 @@ class _PageState extends State<TextLayoutMainPage> {
         ],
       );
 
-
-       ///配置边框圆角
-       switch(backgroundModel){
-         case TextLayoutBackgroundModel.rect:
-         case TextLayoutBackgroundModel.rectBackgroundAndBorder:
-         case TextLayoutBackgroundModel.rectBorder:
-           backGroundRadius = 0;
-           break;
-         case TextLayoutBackgroundModel.rrect:
-         case TextLayoutBackgroundModel.rrectBackgroundAndBorder:
-         case TextLayoutBackgroundModel.rrectBorder:
-           backGroundRadius = textModel.backGroundRadius;
-           break;
-         case TextLayoutBackgroundModel.oval:
-         case TextLayoutBackgroundModel.ovalBackgroundAndBorder:
-         case TextLayoutBackgroundModel.ovalBorder:
-         backGroundRadius =
-             (10 + fontSize + widget.padding.top + widget.padding.bottom +
-                 widget.margins.top + widget.margins.bottom) / 2;
-           break;
-         case TextLayoutBackgroundModel.none:
-           backGroundRadius = 0;
-           break;
-       }
-      Border border ;
-      switch(backgroundModel){
+      ///配置边框圆角
+      switch (backgroundModel) {
+        case TextLayoutBackgroundModel.rect:
+        case TextLayoutBackgroundModel.rectBackgroundAndBorder:
+        case TextLayoutBackgroundModel.rectBorder:
+          backGroundRadius = 0;
+          break;
+        case TextLayoutBackgroundModel.rrect:
+        case TextLayoutBackgroundModel.rrectBackgroundAndBorder:
+        case TextLayoutBackgroundModel.rrectBorder:
+          backGroundRadius = textModel.backGroundRadius;
+          break;
+        case TextLayoutBackgroundModel.oval:
+        case TextLayoutBackgroundModel.ovalBackgroundAndBorder:
+        case TextLayoutBackgroundModel.ovalBorder:
+          backGroundRadius = (10 +
+                  fontSize +
+                  widget.padding.top +
+                  widget.padding.bottom +
+                  widget.margins.top +
+                  widget.margins.bottom) /
+              2;
+          break;
+        case TextLayoutBackgroundModel.none:
+          backGroundRadius = 0;
+          break;
+      }
+      Border border;
+      switch (backgroundModel) {
         case TextLayoutBackgroundModel.rect:
         case TextLayoutBackgroundModel.rrect:
         case TextLayoutBackgroundModel.oval:
-        border = null;
-        break;
+          border = null;
+          break;
 
         case TextLayoutBackgroundModel.rectBorder:
         case TextLayoutBackgroundModel.rrectBorder:
         case TextLayoutBackgroundModel.ovalBorder:
-         border = textModel.border??new Border.all(color:backgroundColor, width: 0.5);
-         backgroundColor=Colors.transparent;
+          border = textModel.border ??
+              new Border.all(color: backgroundColor, width: 0.5);
+          backgroundColor = Colors.transparent;
           break;
 
         case TextLayoutBackgroundModel.rrectBackgroundAndBorder:
         case TextLayoutBackgroundModel.rectBackgroundAndBorder:
         case TextLayoutBackgroundModel.ovalBackgroundAndBorder:
-        border = textModel.border??new Border.all(color:backgroundColor, width: 0.5);
+          border = textModel.border ??
+              new Border.all(color: backgroundColor, width: 0.5);
           break;
         case TextLayoutBackgroundModel.none:
-          backgroundColor=Colors.transparent;
+          backgroundColor = Colors.transparent;
           backGroundRadius = 0;
           break;
       }
@@ -142,8 +168,16 @@ class _PageState extends State<TextLayoutMainPage> {
           border: border,
         ),
       );
-
-      columnList.add(container);
+      if (widget.tagClickCallback != null) {
+        columnList.add(InkWell(
+          onTap: () {
+            widget.tagClickCallback(textModel.text);
+          },
+          child: container,
+        ));
+      } else {
+        columnList.add(container);
+      }
     }
 
     return Container(
@@ -151,7 +185,12 @@ class _PageState extends State<TextLayoutMainPage> {
         top: 10.0,
       ),
       alignment: Alignment.center,
-      child: Wrap(spacing: 8.0, runSpacing: 8.0, children: columnList,alignment: WrapAlignment.spaceEvenly,),
+      child: Wrap(
+        spacing: 8.0,
+        runSpacing: 8.0,
+        children: columnList,
+        alignment: WrapAlignment.spaceEvenly,
+      ),
     );
   }
 
